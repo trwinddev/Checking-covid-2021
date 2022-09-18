6cb4317b73722cec4c8c2c9900a486774bb2fa88
Ngô Trọng Phong <91360534+phongngotrong@users.noreply.github.com>
2022-09-18 13:37:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import Nav from "./views/Nav";
 import { useState, useEffect } from "react";
-import Todo from "./views/Todo";
 import Covid from "./views/Covid";
 
 const App = () => {
@@ -10,11 +8,9 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
         <img src={logo} className="App-logo" alt="logo" />
         <h4>Checking Covid-19 in 2021</h4>
         <Covid></Covid>
-      
       </header>
     </div>
   );
